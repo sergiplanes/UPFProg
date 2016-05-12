04fc168692cb7e846e586783cf154887b0ea74a1
sergislabgg <sergislabgg@kubuntuX555LAB>
2016-05-12 12:03:47 +0200

    Menu.

--- Practica_2/practica_2.cc
+++ Practica_2/practica_2.cc
@@ -28,9 +28,45 @@
  * THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-#include <iostream>
 #include <stdio.h>
 
 int main(int argc, char**argv) {
-    printf("hello world");
+    int menu=0;
+    do{
+        printf("***********Menu inici UPF, expedients***********\n");
+        printf("Les operacions mínimes que ha d’implementar el programa són:\n");
+        printf("1. Carregar fitxers: carrega fitxers existents\n");
+        printf("2. Alta d’expedient: Registra un alumne nou i crea un expedient amb les dades generals però sense assignatures\n");
+        printf("3. Entrar matricula: crea expedients nous i modifica fitxers expedients existents\n");
+        printf("4. Entrar notes manualment: entra totes les notes d’una assignatura (i actualitza fitxers)\n");
+        printf("5. (opcional) Estat dels expedients: per cada expedient en memòria mostra el resum de l’expedient \n i indica en quina situació estroba dins el pla docent que està matriculat (veure secció 1.2)\n");
+        printf("Seleccioni una de les opcions llistades:\n");
+
+        scanf("%d",&menu);
+        switch(menu) {
+            case 1:
+                /*1. Carregar fitxers: carrega fitxers existents*/
+                printf("1");
+                break;
+
+            case 2:
+                /*2. Alta d’expedient: Registra un alumne nou i crea un expedient amb les dades generals però sense assignatures*/
+                break;
+
+            case 3:
+                /*3. Entrar matricula: crea expedients nous i modifica fitxers expedients existents*/
+                break;
+
+            case 4:
+                /*4. Entrar notes manualment: entra totes les notes d’una assignatura (i actualitza fitxers)*/
+                break;
+
+            case 5:
+                /*"5. (opcional) Estat dels expedients: per cada expedient en memòria mostra el resum de l’expedient \n i indica en quina situació estroba dins el pla docent que està matriculat (veure secció 1.2)*/
+                break;
+                
+            default:printf("Opcio incorrecte");
+        }
+    }while(menu!=6);
+    
 }
