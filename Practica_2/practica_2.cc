16e3aa1cde08d42a26362be3cc15b52b584c4b63
sergislabgg <sergislabgg@kubuntuX555LAB>
2016-05-12 12:29:46 +0200

    carregar fitxers ¿?

--- Practica_2/practica_2.cc
+++ Practica_2/practica_2.cc
@@ -30,8 +30,37 @@
 
 #include <stdio.h>
 
+int carregar_fitxers(FILE * expedients,FILE * asignatures){
+    
+        expedients=fopen("expedient_12345.txt","r");   //Obrim el fitxer "estudiants.txt" dins el punter de tipos fitxer f.
+        asignatures=fopen("asgs-get.txt","r");   //Obrim el fitxer "estudiants.txt" dins el punter de tipos fitxer f.
+    
+        if(expedients && asignatures==NULL){                    //Aquesa clausula if, es per comprovar si el fitxer "estudiants.txt"
+            printf("ERROR \n");         //te algun coningut o si el programa el carrega correctament.
+            printf("Revisa el fitxer estudiants.txt");
+
+            return (0);
+        }
+}
+
+void alta_expedient(){
+    
+}
+void entrar_matricula(){
+    
+}
+void entrar_notes_manual(){
+    
+}
+void estat_expedient(){
+    
+}
+
 int main(int argc, char**argv) {
     int menu=0;
+    FILE * expedients;       //Declarem la variable que contindra el fitxer expedients.
+    FILE * asignatures;       //Declarem la variable que contindra el fitxer expedients.
+    
     do{
         printf("***********Menu inici UPF, expedients***********\n");
         printf("Les operacions mínimes que ha d’implementar el programa són:\n");
@@ -46,27 +75,32 @@ int main(int argc, char**argv) {
         switch(menu) {
             case 1:
                 /*1. Carregar fitxers: carrega fitxers existents*/
-                printf("1");
+                carregar_fitxers(expedients,asignatures);
                 break;
 
             case 2:
                 /*2. Alta d’expedient: Registra un alumne nou i crea un expedient amb les dades generals però sense assignatures*/
+                alta_expedient();
                 break;
 
             case 3:
                 /*3. Entrar matricula: crea expedients nous i modifica fitxers expedients existents*/
+                entrar_matricula();
                 break;
 
             case 4:
                 /*4. Entrar notes manualment: entra totes les notes d’una assignatura (i actualitza fitxers)*/
+                entrar_notes_manual();
                 break;
 
             case 5:
                 /*"5. (opcional) Estat dels expedients: per cada expedient en memòria mostra el resum de l’expedient \n i indica en quina situació estroba dins el pla docent que està matriculat (veure secció 1.2)*/
+                estat_expedient();
                 break;
                 
             default:printf("Opcio incorrecte");
         }
     }while(menu!=6);
-    
+    fclose(expedients);
+    fclose(asignatures);
 }
